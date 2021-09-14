171a7532dde81d8b7f5e67b8956f48dec36adec1
suelensalsa <suelen.leaf@gmail.com>
2021-09-14 19:36:13 -0300

    initial commit

---
+++ calculadora-media/script.js
@@ -0,0 +1,36 @@
+let notas = [];
+
+function somaNotas() {
+  // @ts-ignore
+  let strNumber = document.getElementById("notaDigitada").value;
+  let number = Number(strNumber).toFixed(2);
+
+  notas.push(number);
+  document.getElementById("notasSoma").innerHTML = notas.join(" + ");
+
+  let input = document.getElementById("notaDigitada");
+  clearField(input);
+}
+
+function mediaNotas() {
+  somaNotas();
+
+  let notasNumber = notas.map(Number);
+  let soma = notasNumber.reduce((total, value) => {
+    return total + value;
+  });
+
+  let resultado = soma / notas.length;
+
+  document.getElementById("notasMedia").innerHTML = resultado.toFixed(2) + "!";
+}
+
+function resetMedia() {
+  notas = [];
+  document.getElementById("notasSoma").innerHTML = "";
+  document.getElementById("notasMedia").innerHTML = "";
+}
+
+function clearField(input) {
+  input.value = "";
+}
